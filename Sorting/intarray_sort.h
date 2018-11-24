5330c829b39555928857ca39e36f883c6921b84b
tmgreenside <trevmg7@gmail.com>
2018-11-24 14:16:16 -0800

    Completed merge sort

--- Sorting/intarray_sort.h
+++ Sorting/intarray_sort.h
@@ -42,46 +42,54 @@ void copyArray(int array[], int newArray[], int start, int stop) {
 }
 
 void mergeSortHelper(int array[], int working[], int length) {
-    if (length > 2) {
-        int midpoint = length / 2;
-        int split1[midpoint];
-        int work1[midpoint];
+    if (length >= 2) {
+        int breakPoint = length / 2;
+        int left[breakPoint];
+        int leftWork[breakPoint];
+        int right[length - breakPoint];
+        int rightWork[length - breakPoint];
+        copyArrayNew(array, left, 0, breakPoint);
+        copyArrayNew(array, right, breakPoint, length);
+        mergeSortHelper(left, leftWork, breakPoint);
+        mergeSortHelper(right, rightWork, (length - breakPoint));
 
-        int split2[length - midpoint];
-        int work2[length - midpoint];
-
-        copyArrayNew(array, split1, 0, midpoint);
-        copyArrayNew(array, split2, midpoint, length);
-
-        mergeSortHelper(split1, work1, midpoint);
-        mergeSortHelper(split2, work2, length - midpoint);
-
-        int trace1 = 0, trace2 = 0;
-        for (int i = 0; i < length; i++) {
-            if (trace1 == (length - midpoint) || work2[trace2] <= work1[trace1]) {
-                working[i] = work2[trace2++];
+        // merge
+        int traceLeft = 0;
+        int traceRight = 0;
+        int traceWork = 0;
+        while (traceLeft < breakPoint && traceRight < (length - breakPoint)) {
+            if (leftWork[traceLeft] <= rightWork[traceRight]) {
+                working[traceWork] = leftWork[traceLeft++];
             } else {
-                working[i] = work1[trace1++];
+                working[traceWork] = rightWork[traceRight++];
             }
+            traceWork++;
         }
-    } else if (length == 2) {
-        if (array[0] > array[1]) {
-            swap(array[0], array[1]);
+        while (traceLeft < breakPoint) {
+            working[traceWork] = leftWork[traceLeft++];
+            traceWork++;
         }
-        copyArrayNew(array, working, 0, 2);
+        while (traceRight < (length - breakPoint)) {
+            working[traceWork] = rightWork[traceRight++];
+            traceWork++;
+        }
     } else {
-        copyArrayNew(array, working, 0, length);
+        copyArray(array, working, 0, length);
     }
 }
 
 /*
 Used a TutorialsPoint article as basis for this Merge Sort implementation.
+
+How this works:
+Uses recursion to repeatedly break down the array into separate, smaller arrays
+until there is only a length of 1, then
 */
 void mergeSort(int values[], int length) {
     if (length < 2) return;
-    int working[length];
-    mergeSortHelper(values, working, length);
-    copyArray(working, values, 0, length);
+    int result[length];
+    mergeSortHelper(values, result, length);
+    copyArray(result, values, 0, length);
 }
 
 #endif
